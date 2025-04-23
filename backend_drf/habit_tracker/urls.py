28ff772b1cc61c61b0769fe27be77a949e8fd408
SaravanaJap <saravanajagan607@gmail.com>
2025-04-23 22:59:09 +0530

    homepae

--- backend_drf/habit_tracker/urls.py
+++ backend_drf/habit_tracker/urls.py
@@ -16,8 +16,10 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
+import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/',include('api.urls')),
+    path('',views.home , name='home'),
 ]
