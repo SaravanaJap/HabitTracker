b8241d236144a64cfc6a8b8be271d8fe157e9539
SaravanaJap <saravanajagan607@gmail.com>
2025-04-24 22:38:10 +0530

    stat

--- backend_drf/habit_tracker/settings.py
+++ backend_drf/habit_tracker/settings.py
@@ -103,7 +103,7 @@ ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='').split(',')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': config('DB_NAME'),
+        'NAME': 'habitude_db',
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
         'HOST': config('DB_HOST'),
